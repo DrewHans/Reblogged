7b9f42d906a2d8c89d155c929af1138603a80238
DrewHans555 <drewhans555@gmail.com>
2018-09-28 17:44:25 -0500

    Add BlogCoreSetup, Add reference to Blog.Core.csproj to Blog.MVC.csproj

---
+++ src/Blog.MVC/BlogCoreSetup.cs
@@ -0,0 +1,69 @@
+using Blog.Core;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Blog.MVC.Setup
+{
+    internal static class BlogCoreSetup
+    {
+        public static void AddBlogCoreSingletons(IServiceCollection services)
+        {
+            AddSingleton_Builders(services);
+            AddSingleton_FileDataAccess(services);
+            AddSingleton_FileDataAccessAdapters(services);
+            AddSingleton_Repositories(services);
+            AddSingleton_Validators(services);
+        }
+
+        private static void AddSingleton_Builders(IServiceCollection services)
+        {
+            services.AddSingleton<ISqlParameterBuilder, SqlParameterBuilder>();
+        }
+
+        private static void AddSingleton_FileDataAccess(IServiceCollection services)
+        {
+            services.AddSingleton<IFileReader, FileReader>();
+            services.AddSingleton<IFileWriter, FileWriter>();
+            services.AddSingleton(typeof(IFileDataAccess<>), typeof(FileDataAccess<>));
+        }
+
+        private static void AddSingleton_FileDataAccessAdapters(IServiceCollection services)
+        {
+            services.AddSingleton<IBlogPostDataAccessAdapter, BlogPostFileAdapter>();
+            services.AddSingleton<IBlogUserDataAccessAdapter, BlogUserFileAdapter>();
+        }
+
+        private static void AddSingleton_Repositories(IServiceCollection services)
+        {
+            services.AddSingleton<IBlogPostRepository, BlogPostRepository>();
+            services.AddSingleton<IBlogUserRepository, BlogUserRepository>();
+        }
+
+        private static void AddSingleton_SqlServerDataAccess(IServiceCollection services)
+        {
+            services.AddSingleton<ISqlServerDataAccess, SqlServerDataAccess>();
+        }
+
+        private static void AddSingleton_SqlServerDataAccessAdapters(IServiceCollection services)
+        {
+            services.AddSingleton<IBlogPostDataAccessAdapter, BlogPostSqlServerAdapter>();
+            services.AddSingleton<IBlogUserDataAccessAdapter, BlogUserSqlServerAdapter>();
+        }
+
+        private static void AddSingleton_Validators(IServiceCollection services)
+        {
+            services.AddSingleton<IBlogPostValidator, BlogPostValidator>();
+            services.AddSingleton<IBlogUserValidator, BlogUserValidator>();
+        }
+
+        private static void AddSingleton_WebApiDataAccess(IServiceCollection services)
+        {
+            services.AddSingleton<IWebApiDataAccess, WebApiDataAccess>();
+        }
+
+        private static void AddSingleton_WebApiDataAccessAdapters(IServiceCollection services)
+        {
+            services.AddSingleton<IBlogPostDataAccessAdapter, BlogPostWebApiAdapter>();
+            services.AddSingleton<IBlogUserDataAccessAdapter, BlogUserWebApiAdapter>();
+        }
+    }
+}
--- src/Blog.MVC/Startup.cs
+++ src/Blog.MVC/Startup.cs
@@ -1,11 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -14,28 +9,32 @@ namespace Blog.MVC
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; }
+
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
         }
 
-        public IConfiguration Configuration { get; }
-
-        // This method gets called by the runtime. Use this method to add services to the container.
+        // This method gets called by the runtime.
+        // Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.Configure<CookiePolicyOptions>(options =>
             {
-                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
+                // This lambda determines whether user consent for 
+                // non-essential cookies is needed for a given request.
                 options.CheckConsentNeeded = context => true;
                 options.MinimumSameSitePolicy = SameSiteMode.None;
             });
 
+            Blog.MVC.Setup.BlogCoreSetup.AddBlogCoreSingletons(services);
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        // This method gets called by the runtime.
+        // Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             if (env.IsDevelopment())
