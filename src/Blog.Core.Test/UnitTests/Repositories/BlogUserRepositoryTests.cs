6e671e7d9b1336d4f3c3c8c9d57981ca5668eb66
DrewHans555 <drewhans555@gmail.com>
2018-09-28 08:42:11 -0500

    Add Return Tests to RepositoryTests

--- src/Blog.Core.Test/UnitTests/Repositories/BlogUserRepositoryTests.cs
+++ src/Blog.Core.Test/UnitTests/Repositories/BlogUserRepositoryTests.cs
@@ -1,10 +1,22 @@
 using Blog.Core.Test.Fakes;
+using System.Collections.Generic;
 using Xunit;
 
 namespace Blog.Core.Test
 {
     public class BlogUserRepositoryTests
     {
+        [Fact]
+        public void Add_ValidBlogUser_Returns()
+        {
+            var stubDataAccessAdapter = new StubIBlogUserDataAccessAdapter();
+            var stubValidator = new StubIBlogUserValidator();
+            var repository = new BlogUserRepository(stubDataAccessAdapter, stubValidator);
+            var param_blogUser = new BlogUserFactory().Create();
+
+            repository.Add(param_blogUser);
+        }
+
         [Fact]
         public void Add_ValidBlogUser_VerifyValidator()
         {
@@ -29,6 +41,17 @@ namespace Blog.Core.Test
             repository.Add(param_blogUser);
 
             mockDataAccessAdapter.VerifyAdd(param_blogUser);
+        }
+
+        [Fact]
+        public void Delete_ValidBlogUser_Returns()
+        {
+            var stubDataAccessAdapter = new StubIBlogUserDataAccessAdapter();
+            var stubValidator = new StubIBlogUserValidator();
+            var repository = new BlogUserRepository(stubDataAccessAdapter, stubValidator);
+            var param_blogUser = new BlogUserFactory().Create();
+
+            repository.Delete(param_blogUser);
         }
 
         [Fact]
@@ -58,6 +81,17 @@ namespace Blog.Core.Test
         }
 
         [Fact]
+        public void Edit_ValidBlogUser_Returns()
+        {
+            var stubDataAccessAdapter = new StubIBlogUserDataAccessAdapter();
+            var stubValidator = new StubIBlogUserValidator();
+            var repository = new BlogUserRepository(stubDataAccessAdapter, stubValidator);
+            var param_blogUser = new BlogUserFactory().Create();
+
+            repository.Edit(param_blogUser);
+        }
+
+        [Fact]
         public void Edit_ValidBlogUser_VerifyValidator()
         {
             var stubDataAccessAdapter = new StubIBlogUserDataAccessAdapter();
@@ -84,6 +118,21 @@ namespace Blog.Core.Test
         }
 
         [Fact]
+        public void GetById_ValidBlogUser_ReturnsExpectedBlogUser()
+        {
+            var stubDataAccessAdapter = new StubIBlogUserDataAccessAdapter();
+            var stubValidator = new StubIBlogUserValidator();
+            var repository = new BlogUserRepository(stubDataAccessAdapter, stubValidator);
+            var expected = new BlogUserFactory().Create();
+            stubDataAccessAdapter.StubGetById(expected);
+            var param_userId = expected.UserId;
+
+            var actual = repository.GetById(param_userId);
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
         public void GetById_ValidBlogUser_VerifyDataAccessAdapter()
         {
             var mockDataAccessAdapter = new MockIBlogUserDataAccessAdapter();
@@ -94,6 +143,20 @@ namespace Blog.Core.Test
             repository.GetById(param_userId);
 
             mockDataAccessAdapter.VerifyGetById(param_userId);
+        }
+
+        [Fact]
+        public void List_ValidBlogUser_ReturnsExpectedList()
+        {
+            var stubDataAccessAdapter = new StubIBlogUserDataAccessAdapter();
+            var stubValidator = new StubIBlogUserValidator();
+            var repository = new BlogUserRepository(stubDataAccessAdapter, stubValidator);
+            var expected = new List<BlogUser> { new BlogUserFactory().Create() };
+            stubDataAccessAdapter.StubList(expected);
+
+            var actual = repository.List();
+
+            Assert.Equal(expected, actual);
         }
 
         [Fact]
