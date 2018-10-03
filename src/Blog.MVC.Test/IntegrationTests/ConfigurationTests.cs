bf0ea16ba701fe6621bd6cf04e99f80f2f0504dc
DrewHans555 <drewhans555@gmail.com>
2018-10-02 20:47:47 -0500

    Add Configuration Integration Tests (to verify usersecret access)

---
+++ src/Blog.MVC.Test/IntegrationTests/ConfigurationTests.cs
@@ -0,0 +1,51 @@
+using Microsoft.Extensions.Configuration;
+using Xunit;
+
+namespace Blog.MVC.Test
+{
+    internal class FakeStartup
+    {
+        public IConfiguration Configuration { get; }
+        public FakeStartup()
+        {
+            var builder = new ConfigurationBuilder();
+            builder.AddUserSecrets<FakeStartup>();
+            Configuration = builder.Build();
+        }
+    }
+
+    public class ConfigurationTests
+    {
+        [Fact]
+        public void InitFakeStartup_DefaultConstructorCall_ReturnsInstance()
+        {
+            var fakestartup = new FakeStartup();
+            Assert.NotNull(fakestartup);
+        }
+
+        [Fact]
+        public void UseConfig_ValidConfigKey_ReturnsExpectedConfigValue()
+        {
+            var fakestartup = new FakeStartup();
+            var configuration = fakestartup.Configuration;
+            var expected = "TestValue"; // a real config value in secrets.json
+            var param_key = "TestKey"; // a real config key in secrets.json
+
+            var actual = configuration[param_key];
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void UseConfig_InvalidConfigKey_ReturnsNull()
+        {
+            var fakestartup = new FakeStartup();
+            var configuration = fakestartup.Configuration;
+            var param_key = "InvalidKeyNotInUserSecrets";
+
+            var returned = configuration[param_key];
+
+            Assert.Null(returned);
+        }
+    }
+}
